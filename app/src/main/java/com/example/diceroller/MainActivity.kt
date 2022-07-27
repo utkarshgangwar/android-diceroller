47b0f28650b410e88edd6dd76ba8287a5806d0b6
Utkarsh Gangwar <utkarshgangwar22@gmail.com>
2022-07-27 15:21:03 +0530

    version 0.2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 import android.content.Context
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.*
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -23,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
-            Toast.makeText(this, getString(R.string.rolledToast), Toast.LENGTH_SHORT).show()
         }
         diceImage = findViewById(R.id.dice_image)
     }
@@ -49,7 +50,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableResourse)
-
+        Toast.makeText(this, getString(R.string.rolledToast), Toast.LENGTH_SHORT).show()
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -69,5 +70,32 @@ class MainActivity : AppCompatActivity() {
             @Suppress("DEPRECATION")
             vib.vibrate(50)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id==R.id.menu_about) {
+            Toast.makeText(this,"About Clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity,AboutActivity::class.java)
+            startActivity(intent)
+        }
+        if (id==R.id.menu_share) {
+            Toast.makeText(this,"Share Clicked", Toast.LENGTH_SHORT).show()
+            try {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Dice Rolling Offline")
+                var shareMessage = "\nLet me recommend you this application\n\n"
+                shareMessage =
+                    """
+                    ${shareMessage}https://drive.google.com/file/d/1vi5IkDbKhAoiCIBejFO-VcCR26yrxa0f/view?usp=sharing${BuildConfig.APPLICATION_ID}
+                    """.trimIndent()
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+                startActivity(Intent.createChooser(shareIntent, "choose one"))
+            } catch (e: Exception) {
+                //e.toString();
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
